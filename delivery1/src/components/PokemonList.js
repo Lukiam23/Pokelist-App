21cb6834d1272654403aa3024a9bea95f83b524c
Lukiam23 <mscorreia95@gmail.com>
2022-03-21 11:37:24 -0300

    redefinir useState

--- delivery1/src/components/PokemonList.js
+++ delivery1/src/components/PokemonList.js
@@ -3,16 +3,13 @@ import Card from "./Card";
 import style from "../css/PokemonList.module.css";
 
 function PokemonList(){
-    const [pokemonList, setPokemonList] = useState([])
-    useEffect(() => {
-            setPokemonList([ 'Arcanine.png','Beedrill.png', 'Blastoise.png','Bulbasaur.png', 
-            'Butterfree.png','Caterpie.png', 'Charizard.png','Charmander.png', 'Charmeleon.png',
-            'Clefable.png', 'Dewgong.png',  'Farfetchd.png', 'Golbat.png', 'Ivysaur.png', 
-            'Kadabra.png',  'Kakuna.png', 'Metapod.png', 'Pidgeot.png', 'Pidgeotto.png','Pidgey.png', 
-            'Pikachu.png',  'Ponyta.png', 'Raichu.png', 'Raticate.png', 'Rattata.png',  'Squirtle.png', 
-            'Venusaur.png', 'Vulpix.png', 'Wartortle.png','Weedle.png'])
-
-    },[]);
+    const [pokemonList, setPokemonList] = useState([ 'Arcanine.png','Beedrill.png', 'Blastoise.png','Bulbasaur.png', 
+    'Butterfree.png','Caterpie.png', 'Charizard.png','Charmander.png', 'Charmeleon.png',
+    'Clefable.png', 'Dewgong.png',  'Farfetchd.png', 'Golbat.png', 'Ivysaur.png', 
+    'Kadabra.png',  'Kakuna.png', 'Metapod.png', 'Pidgeot.png', 'Pidgeotto.png','Pidgey.png', 
+    'Pikachu.png',  'Ponyta.png', 'Raichu.png', 'Raticate.png', 'Rattata.png',  'Squirtle.png', 
+    'Venusaur.png', 'Vulpix.png', 'Wartortle.png','Weedle.png'])
+    
 
     return(
         <div className={style.container}>
