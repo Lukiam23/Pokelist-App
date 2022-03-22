9fb2f158ebeaa7ce36b4ba8aba9bbc31f813a3af
Lukiam23 <mscorreia95@gmail.com>
2022-03-22 09:35:12 -0300

    starting delivery 2

--- delivery1/src/App.js
+++ delivery1/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import PokemonList from './components/PokemonList'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>
+    <>
+      <Header />
       <PokemonList />
-    </div>
+    </>
   );
 }
 
---
+++ delivery1/src/components/Header.js
@@ -0,0 +1,12 @@
+import style from "../css/Header.module.css"
+function Header(){
+    return(
+        <header>
+            <nav>
+               <a href="/">Add Pokemon</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
